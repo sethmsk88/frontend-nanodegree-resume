651aec486806aeddd2f754a570fcbbf5ff75d555
Seth Kerr <sethmsk88@yahoo.com>
2016-01-05 17:11:07 -0500

    Complete Problem Set 0

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,10 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+var c = 299792458; // speed of light (m/s)
+
+var c_nm_per_sec = c * 100 / 1000000000;
+
+$("#main").append("Seth Kerr<br />");
+$("#main").append(c_nm_per_sec + " cm/nanosec<br />");
